8856950f6e07e051a9ebebf85c6c9b963a5185d1
geoffrey-taylor <gdt42@protonmail.com>
2025-10-22 11:44:57 -0700

    Start search results skeleton

---
+++ src/SearchResults/SearchResults.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+
+//Skeleton for dummy search results section
+function SearchResults() {
+    const [results, setResults] = useState([]);
+
+};
+
+export default SearchResults;
+
+//Array or List???
